c509e746066f4615b784d65257653cb013150498
Prasanna Santhanam <tsp@qubole.com>
2016-02-25 15:03:08 +0530

    applying @umangd patch for sqlite based retries of failed amis

--- builder/amazon/ebs/step_create_ami.go
+++ builder/amazon/ebs/step_create_ami.go
@@ -1,9 +1,10 @@
 package ebs
 
 import (
+	"database/sql"
 	"fmt"
-
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/mattn/go-sqlite3"
 	"github.com/mitchellh/multistep"
 	awscommon "github.com/mitchellh/packer/builder/amazon/common"
 	"github.com/mitchellh/packer/packer"
@@ -17,6 +18,7 @@ func (s *stepCreateAMI) Run(state multistep.StateBag) multistep.StepAction {
 	config := state.Get("config").(Config)
 	ec2conn := state.Get("ec2").(*ec2.EC2)
 	instance := state.Get("instance").(*ec2.Instance)
+	image := state.Get("source_image").(*ec2.Image)
 	ui := state.Get("ui").(packer.Ui)
 
 	// Create the image
@@ -57,6 +59,34 @@ func (s *stepCreateAMI) Run(state multistep.StateBag) multistep.StepAction {
 		return multistep.ActionHalt
 	}
 
+	// In the following block of code we write the AMI id generated for the region into a
+	// local sqlite db. This allows us to retry AMI baking only for regions which do not have
+	// a valid AMI entry in the db
+	var ami_type string
+	if "paravirtual" == *image.VirtualizationType {
+		ami_type = "pv"
+	} else {
+		ami_type = "hvm"
+	}
+
+	ui.Say("AMI created. Updating the database now.")
+	var DB_DRIVER string
+	sql.Register(DB_DRIVER, &sqlite3.SQLiteDriver{})
+	db, err := sql.Open(DB_DRIVER, "pacman.db")
+	checkErr(err, state, "failed to create the database handle")
+
+	stmt, err := db.Prepare("update bake_ami set ami_status=1, ami_id=? where region=? and ami_type=?")
+	checkErr(err, state, "preparing update query failed")
+
+	res, err := stmt.Exec(createResp.ImageId, *ec2conn.Config.Region, ami_type)
+	checkErr(err, state, "update execution failed")
+
+	affect, err := res.RowsAffected()
+	checkErr(err, state, "update db failed")
+
+	ui.Say(fmt.Sprintf("Updated database with %d row(s) affected", affect))
+	db.Close()
+
 	imagesResp, err := ec2conn.DescribeImages(&ec2.DescribeImagesInput{ImageIds: []*string{createResp.ImageId}})
 	if err != nil {
 		err := fmt.Errorf("Error searching for AMI: %s", err)
@@ -67,6 +97,15 @@ func (s *stepCreateAMI) Run(state multistep.StateBag) multistep.StepAction {
 	s.image = imagesResp.Images[0]
 
 	return multistep.ActionContinue
+}
+
+func checkErr(err error, state multistep.StateBag, msg string) {
+	if err != nil {
+		err := fmt.Errorf(msg)
+		state.Put("error", err)
+		ui := state.Get("ui").(packer.Ui)
+		ui.Error(err.Error())
+	}
 }
 
 func (s *stepCreateAMI) Cleanup(state multistep.StateBag) {
